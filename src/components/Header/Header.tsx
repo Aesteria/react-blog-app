35bb062d6efc5e2fa6cac4ff174c3493fe013f73
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-02-11 14:20:00 +0300

    change a tag to Link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import HeaderLoggedIn from './HeaderLoggedIn';
 import HeaderLoggedOut from './HeaderLoggedOut';
 
@@ -12,10 +13,10 @@ const Header = ({ loggedIn, onLogout, onLogin }: HeaderProps) => {
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
         <h4 className="my-0 mr-md-auto font-weight-normal">
-          <a href="/" className="text-white">
+          <Link to="/" className="text-white">
             {' '}
             ComplexApp{' '}
-          </a>
+          </Link>
         </h4>
         {loggedIn ? (
           <HeaderLoggedIn onLogout={onLogout} />
