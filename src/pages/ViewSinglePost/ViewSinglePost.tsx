862c3d5b1ff8eeff70d9c84715df1fb88def9197
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-02-20 18:00:45 +0300

    fetch data from a server in post view screen

--- src/pages/ViewSinglePost/ViewSinglePost.tsx
+++ src/pages/ViewSinglePost/ViewSinglePost.tsx
@@ -1,10 +1,43 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Post } from '../../types/Post';
+import { formatDate } from '../../utils/formatDate';
 import Page from '../Page/Page';
 
 const ViewSinglePost = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState<Post>({} as Post);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await axios.get<Post>(`/post/${id}`);
+        setPost(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log('There was a problem with fetching Post');
+      }
+    };
+
+    fetchPost();
+  }, [id]);
+
+  if (isLoading) {
+    return (
+      <Page title="...">
+        <div>Loading...</div>
+      </Page>
+    );
+  }
+
+  const dateFormatted = formatDate(post.createdDate);
+
   return (
     <Page title="Fake Post Title">
       <div className="d-flex justify-content-between">
-        <h2>Example Post Title</h2>
+        <h2>{post.title}</h2>
         <span className="pt-2">
           <a href="#" className="text-primary mr-2" title="Edit">
             <i className="fas fa-edit"></i>
@@ -16,32 +49,21 @@ const ViewSinglePost = () => {
       </div>
 
       <p className="text-muted small mb-4">
-        <a href="#">
+        <Link to={`/profile/${post.author.username}`}>
           <img
             className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
+            src={post.author.avatar}
             alt="user avatar"
           />
-        </a>
-        Posted by <a href="#">brad</a> on 2/10/2020
+        </Link>
+        Posted by{' '}
+        <Link to={`/profile/${post.author.username}`}>
+          {post.author.username}
+        </Link>{' '}
+        on {dateFormatted}
       </p>
 
-      <div className="body-content">
-        <p>
-          Lorem ipsum dolor sit <strong>example</strong> post adipisicing elit.
-          Iure ea at esse, tempore qui possimus soluta impedit natus voluptate,
-          sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat
-          asperiores at.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod
-          asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab
-          exercitationem eligendi culpa explicabo nulla tempora rem? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Iure ea at esse, tempore
-          qui possimus soluta impedit natus voluptate, sapiente saepe modi est
-          pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-        </p>
-      </div>
+      <div className="body-content">{post.body}</div>
     </Page>
   );
 };
