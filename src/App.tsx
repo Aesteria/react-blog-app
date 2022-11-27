cead4e9b19176c9bf2f7409c4d40690261b9145c
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-11-27 19:28:23 +0300

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
 export default function App() {
+  const [a, seta] = useState('test');
+  useEffect(() => {
+    if (a) {
+      console.log('sad');
+    }
+  }, []);
+
   return <div>App</div>;
 }
