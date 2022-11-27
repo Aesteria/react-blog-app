dfab216c7876c9af8b36da9399dbf357d1acb560
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-11-27 20:38:27 +0300

    setup eslint airbnb config with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import React, { useEffect, useState } from 'react';
-
 export default function App() {
-  const [a, seta] = useState('test');
-  useEffect(() => {
-    if (a) {
-      console.log('sad');
-    }
-  }, []);
-
   return <div>App</div>;
 }
