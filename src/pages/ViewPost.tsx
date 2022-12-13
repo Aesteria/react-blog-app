12d5283542223c19df8383fcfd2c6543d91aa06a
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-12-13 17:31:51 +0300

    style view post component

--- src/pages/ViewPost.tsx
+++ src/pages/ViewPost.tsx
@@ -1,7 +1,9 @@
+import { CalendarIcon } from '@heroicons/react/24/outline';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
 import { useParams } from 'react-router-dom';
 import Container from '../components/Container';
+import UserAvatarImage from '../components/UserAvatarImage';
 import { useAppSelector } from '../store/hooks';
 import { selectPostById } from '../store/postsSlice';
 import formatDate from '../utils/formatDate';
@@ -16,15 +18,22 @@ export default function ViewPost() {
     return <h2>Post Not Found!</h2>;
   }
 
-  const date = formatDate(post?.createdDate);
+  const date = formatDate(post.createdDate);
 
   return (
-    <div className="post-view">
-      <Container>
-        <h2>{post.title}</h2>
-        <h4 className="font-normal text-sm mb-3">Posted on: {date}</h4>
-        <img src={post.coverImage} alt="Cover" />
-        <div className="post-content">
+    <div className="py-16">
+      <Container className="max-w-5xl">
+        <img src={post.coverImage} alt="Cover" className="mb-8" />
+        <div className="flex items-center mb-8">
+          <div className="flex items-center mr-4">
+            <UserAvatarImage size="small" src={post.author.photoURL} />
+            <span className="ml-2">{post.author.username}</span>
+          </div>
+          <CalendarIcon className="h-6 w-6 text-fuchsia-500" />
+          <span className="ml-2">{date}</span>
+        </div>
+        <h2 className="mb-8 text-3xl font-light">{post.title}</h2>
+        <div className="post-view">
           <ReactQuill theme="bubble" readOnly value={post.body} />
         </div>
       </Container>
