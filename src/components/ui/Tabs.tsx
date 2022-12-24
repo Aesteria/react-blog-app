6d2de304cb43d99d995eae845f2221aa355e8670
Aesteria <fjgjsfasffalfs@yandex.ru>
2022-12-24 16:27:02 +0300

    fix: styles in tabs

--- src/components/ui/Tabs.tsx
+++ src/components/ui/Tabs.tsx
@@ -8,28 +8,24 @@ type TabsProps = {
 export default function Tabs({ tabs }: TabsProps) {
   return (
     <div>
-      <div className="hidden sm:block">
-        <div className="border-b border-gray-200">
-          <nav className="-mb-px flex justify-center" aria-label="Tabs">
-            {tabs.map((tab) => (
-              <NavLink
-                key={tab.name}
-                to={tab.to}
-                className={({ isActive }) =>
-                  clsx(
-                    isActive
-                      ? 'border-indigo-500 text-indigo-600'
-                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                    'w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm'
-                  )
-                }
-              >
-                {tab.name} {tab.count ?? ''}
-              </NavLink>
-            ))}
-          </nav>
-        </div>
-      </div>
+      <nav className="flex justify-between sm:justify-center" aria-label="Tabs">
+        {tabs.map((tab) => (
+          <NavLink
+            key={tab.name}
+            to={tab.to}
+            className={({ isActive }) =>
+              clsx(
+                isActive
+                  ? 'border-indigo-500 text-indigo-600'
+                  : 'border-gray-200 text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                'w-1/4 py-4 px-1 text-center border-b-2 font-medium text-xs sm:text-sm'
+              )
+            }
+          >
+            {tab.name} {tab.count ?? ''}
+          </NavLink>
+        ))}
+      </nav>
     </div>
   );
 }
